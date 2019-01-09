9955c056d53cdb0d25a1a2677be9a844c4631cf4
Ramon Rivera Llavona <rllavona13@me.com>
2019-01-09 13:55:24 -0400

    Adding new style

--- gpon_configurator.py
+++ gpon_configurator.py
@@ -7,8 +7,25 @@ app = Flask(__name__)
 def gpon_form():
 
     if 'submit' in request.form:
-        return render_template('config.html')
+        slot = (request.form['SlotPosition'])
+        olt = (request.form['OLTPosition'])
+        ont = (request.form['ONTPosition'])
+        sernoid = (request.form['SernoID'])
+        customername = (request.form['CustomerName'])
+        pppoeusername = (request.form['pppoeusername'])
+        pppoepassword = (request.form['pppoepassword'])
+        sipuser1 = (request.form['sipuser1'])
+        sippassword1 = (request.form['sippassword1'])
+        sipuser2 = (request.form['sipuser2'])
+        sippassword2 = (request.form['sippassword2'])
+
+        gpon_lines = slot, olt, ont, sernoid, customername, pppoeusername, pppoepassword, sipuser1, sippassword1, \
+            sipuser2, sippassword2
+        return render_template('config.html', lines=gpon_lines)
+
+    else:
+        return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port=8080, debug=True)

ba9f87d4c0a770add84f1d7160057314a3f92ef8
Ramon Rivera <rllavona13@me.com>
2019-01-09 20:19:32 -0500

    HTML Tags fixed and added Zhone ONT Model

--- gpon_configurator.py
+++ gpon_configurator.py
@@ -1,5 +1,9 @@
+__AUTHOR__ = 'Ramon Rivera Llavona'
+"""
+This is a simple script that gives you configuration lines to configure Zhone GPON CPE's
+
+"""
 from flask import Flask, render_template, request
-import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -10,55 +14,57 @@ def gpon_form():
     if 'submit' in request.form:
 
         gpon_position = '\nonu set ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
-                        + '/' + (request.form['ONTPosition']) + ' ' + 'meprof zhone-2426' ' ' \
-                        + (request.form['SernoID']) + '\n'
+                        + '/' + (request.form['ONTPosition']) + ' ' + 'meprof zhone-' \
+                        + (request.form['ONTModel']) + ' ' \
+                        + (request.form['SernoID']) + '\n'  # SLOT NUMBER
 
         gpon_traps = 'onu traps enable ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
-                     + '/' + (request.form['ONTPosition']) + ' phy,ont,line' + '\n'
+                     + '/' + (request.form['ONTPosition']) + ' phy,ont,line' + '\n'  # ENABLE TRAPS IN THE ONU
 
         gpon_com_prof = 'cpe system add ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
-                        + '/' + (request.form['ONTPosition']) + ' sys-common-profile Default' + '\n'
+                        + '/' + (request.form['ONTPosition']) + ' sys-common-profile Default' + '\n'  # SYSTEM PROFILE
 
         gpon_identity = 'cpe system sysinfo add ' + (request.form['SlotPosition']) + '/' \
                         + (request.form['OLTPosition']) \
                         + '/' + (request.form['ONTPosition']) \
                         + ' name "' + (request.form['CustomerName']) + '" location '  \
                         + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
-                        + '/' + (request.form['ONTPosition'])
+                        + '/' + (request.form['ONTPosition'])  # CONFIGURE IDENTITY IN THE CPE
 
         gpon_pppoe_bridge = '\nbridge add 1-1-' + (request.form['OLTPosition']) + '-' + (request.form['ONTPosition']) \
                             + '/gpononu gtp 5 downlink-pppoe ' \
                             + 'vlan 351 tagged eth all wlan 1 ' \
                             + 'rg-bpppoe name ' \
-                            + (request.form['CustomerName']) + '-PPPoE' + '\n'
+                            + (request.form['CustomerName']) + '-PPPoE' + '\n'  # CREATE BRIDGE FOR PPPoE
 
         gpon_pppoe = 'cpe rg wan modify ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
                      + '/' + (request.form['ONTPosition']) + ' pppoe-usr-id ' + (request.form['pppoeusername']) \
-                     + ' pppoe-password ' + (request.form['pppoepassword']) + ' pppoe-auth pap' + '\n'
+                     + ' pppoe-password ' + (request.form['pppoepassword']) + ' pppoe-auth pap' + '\n'  # CONFIGURE
+                                                                                                        # PPPOE ACCOUNT
 
-        gpon_lan = 'cpe rg lan modify 1/4/2 eth 1 ip-addr 192.168.23.1' + '\n'
+        gpon_lan = 'cpe rg lan modify 1/4/2 eth 1 ip-addr 192.168.23.1' + '\n'  # CONFIGURE LAN GATEWAY
 
         gpon_sip_bridge = '\nbridge add 1-1-' + (request.form['OLTPosition']) + '-' + (request.form['ONTPosition']) \
-                            + '/gpononu gtp 1100000001 downlink-voice vlan 10 tagged ' \
-                            + 'sip rg-bridged name "' \
-                            + (request.form['CustomerName']) + '-VOICE"' + '\n'
+                          + '/gpononu gtp 1100000001 downlink-voice vlan 10 tagged ' \
+                          + 'sip rg-bridged name "' \
+                          + (request.form['CustomerName']) + '-VOICE"' + '\n' # CONFIGURE BRIDGE FOR VOIP
 
         gpon_sip1 = 'cpe voip add ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
                     + '/' + (request.form['ONTPosition']) + '/1' + ' dial-number ' + (request.form['sipuser1']) \
                     + ' username ' + (request.form['sipuser1']) + ' password ' + (request.form['sippassword1']) \
-                    + ' voip-server-profile 1' + '\n'
+                    + ' voip-server-profile 1' + '\n'  # CONFIGURE SIP ACCOUNT ON POTS 1
 
         gpon_sip2 = 'cpe voip add ' + (request.form['SlotPosition']) + '/' + (request.form['OLTPosition']) \
                     + '/' + (request.form['ONTPosition']) + '/2' + ' dial-number ' + (request.form['sipuser2']) \
                     + ' username ' + (request.form['sipuser2']) + ' password ' + (request.form['sippassword2']) \
-                    + ' voip-server-profile 1' + '\n'
+                    + ' voip-server-profile 1' + '\n'  # CONFIGURE SIP ACCOUNT ON POTS 2
 
         return render_template('config.html', gpon_lines=gpon_position + gpon_traps + gpon_com_prof
                                + gpon_identity, gpon_pppoe=gpon_pppoe_bridge + gpon_pppoe + gpon_lan,
                                gpon_voice=gpon_sip_bridge + gpon_sip1 + gpon_sip2)
 
     else:
-        return render_template('index.html')
+        return render_template('index.html')  # WHEN SCRIPT RUNS, THIS IS THE PAGE IT SHOWS UNTIL YOU SUBMIT THE FORM
 
 
 if __name__ == '__main__':
